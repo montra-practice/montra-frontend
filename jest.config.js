bedd73385ba78e55903e4e1238fd1b5cf56af3a7
Cara_Wu <15902804674@163.com>
2022-06-22 16:54:48 +0800

    [TECH]: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  rootDir: "<rootDir>/src",
+  rootDir: '<rootDir>/src',
   setupFiles: [
-    "react-app-polyfill/jsdom"
+    'react-app-polyfill/jsdom'
   ],
   setupFilesAfterEnv: [
-    "<rootDir>/src/setupTests.ts"
+    '<rootDir>/src/setupTests.ts'
   ],
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
   ],
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    "^react-native$": "react-native-web",
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
   },
   moduleFileExtensions: [
     'js',
@@ -23,25 +23,25 @@ module.exports = {
     'jsx'
   ],
   transform: {
-    "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/config/jest/babelTransform.js",
-    "^.+\\.(css|scss)$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
+    '^.+\\.(css|scss)$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
   },
   transformIgnorePatterns: [
-    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
-    "^.+\\.module\\.(css|sass|scss)$"
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$'
   ],
   collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.d.ts",
-    "!**/node_modules/**",
-    "!**/apis/**"
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/apis/**'
   ],
   collectCoverage: true,
-  coverageReporters: ["text-summary"],
+  coverageReporters: ['text-summary'],
   watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
   ],
   resetMocks: true
 }
