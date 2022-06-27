2bfb5814215441d17f4e3dc87c438100f67cd02d
Shan_Wu <shan_wu@epam.com>
2022-06-27 17:27:51 +0800

    [Login]: fixed landing unit test

--- src/pages/Landing/index.test.tsx
+++ src/pages/Landing/index.test.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import Landing from './index'
 
 describe('test Landing page', () => {
+  let history: MemoryHistory
+  beforeEach(() => {
+    history = createMemoryHistory()
+  })
+
   test('renders swipe default title', () => {
-    render(<Landing />)
+    render(
+      <Router location={history.location} navigator={history}>
+        <Landing />
+      </Router>,
+    )
     const textElement = screen.getByText(/gain total control of your money/i)
     expect(textElement).toBeInTheDocument()
   })
 
   test('renders sign up & login buttons', () => {
-    render(<Landing />)
+    render(
+      <Router location={history.location} navigator={history}>
+        <Landing />
+      </Router>,
+    )
     const signUpElement = screen.getByRole('button', { name: 'Sign Up' })
     const loginElement = screen.getByRole('button', { name: 'Login' })
 
@@ -21,7 +34,7 @@ describe('test Landing page', () => {
   })
 
   test('go to sign up page when user click Sign Up button', () => {
-    const history = createMemoryHistory()
+    // const history = createMemoryHistory()
     render(
       <Router location={history.location} navigator={history}>
         <Landing />
