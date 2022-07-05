ba2c4a0d59d3b8bd5f5eece3257271acbd009ded
Shan_Wu <shan_wu@epam.com>
2022-07-05 14:55:37 +0800

    [Sign Up]: remove unnecessary code in jest setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,29 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-// import * as jest from 'jest'
 import Schema from 'async-validator'
 
 const originError = console.error
-
-// remove px tester warning
-// export function excludeWarning() {
-//   const errorSpy = jest.spyOn(console, 'error')
-//     .mockImplementation((msg, ...rest) => {
-//       if (
-//         String(msg).includes(
-//           'The px tester is not rendering properly. Please make sure you have imported `antd-mobile/es/global`.'
-//         )
-//       ) {
-//         return
-//       }
-//       originError(msg, ...rest)
-//     })
-//
-//   return () => {
-//     errorSpy.mockRestore()
-//   }
-// }
 
 Schema.warning = jest.fn()
 
