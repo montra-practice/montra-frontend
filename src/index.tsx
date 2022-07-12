e09f98ef7f0367b8d96ac19173b7c5f1e1dc2eeb
Maxer Wu <maxey_wu@epam.com>
2022-07-12 17:43:33 +0800

    feat: add userProfile page

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/style/reset.scss'
 import '@/style/overwrite.scss'
@@ -6,11 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/UserProfile/router.tsx
@@ -0,0 +1,6 @@
+import { Route } from 'react-router'
+import UserProfile from './index'
+
+export default [
+  <Route path="/user-profile" element={<UserProfile />} key="userProfile" />,
+]
