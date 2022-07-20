562dbf0958f8bb99d7829012d2e447c427714980
Cara_Wu <15902804674@163.com>
2022-07-20 18:12:06 +0800

    [TECH]: fixed transaction unit test

--- src/pages/Transaction/index.test.tsx
+++ src/pages/Transaction/index.test.tsx
@@ -1,12 +1,20 @@
 import { screen, render } from '@testing-library/react'
 import Transaction from '.'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory, MemoryHistory } from 'history'
 
 describe('test transaction page', () => {
-  describe('render page UI', () => {
-    it('should rend financial guide btn', () => {
-      render(<Transaction />)
+  let history: MemoryHistory
+  beforeEach(() => {
+    history = createMemoryHistory()
+  })
+  it('should rend financial guide btn', () => {
+    render(
+      <Router location={history.location} navigator={history}>
+        <Transaction />
+      </Router>,
+    )
 
-      expect(screen.getByText(/see you financial report/i)).toBeInTheDocument()
-    })
+    expect(screen.getByText(/see you financial report/i)).toBeInTheDocument()
   })
 })
