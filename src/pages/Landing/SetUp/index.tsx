564dbdd9f643c302a3de2a56f809fb829214c878
Cara_Wu <15902804674@163.com>
2022-07-20 15:09:23 +0800

    [Login]: setup ui style

--- src/pages/Landing/SetUp/index.tsx
+++ src/pages/Landing/SetUp/index.tsx
@@ -1,9 +1,14 @@
+import { Button } from 'antd-mobile'
 import styles from './index.scss'
 
 export default () => {
   return (
     <div className={styles.wrapper}>
       <h2>Let’s setup your account!</h2>
+      <p>Account can be your bank, credit card or your wallet.</p>
+      <Button color="primary" block size="large">
+        Let's go
+      </Button>
     </div>
   )
 }
