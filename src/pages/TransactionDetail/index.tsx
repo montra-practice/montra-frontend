539fda81b5485402938b53cf7308d2c2239fce08
anna_gan <anna_gan@epam.com>
2022-08-05 12:37:28 +0800

    feat: add TransactionDetail UI

--- src/pages/TransactionDetail/index.tsx
+++ src/pages/TransactionDetail/index.tsx
@@ -3,6 +3,7 @@ import RemoveIcon from '@/assets/icons/remove.png'
 import { useNavigate } from 'react-router'
 import RemoveAlert from '@/components/RemoveAlert'
 import DialogShow from '@/components/DialogShow'
+import ControlMoneyImg from '@/assets/images/control_money.png'
 import styles from './index.scss'
 import { useState } from 'react'
 
@@ -18,7 +19,9 @@ export default () => {
   }
   const handleRemoveConfirm = () => {
     handleRemoveVisible()
-    DialogShow('Remove successfully')
+    DialogShow('Remove successfully', () => {
+      navigate('/transaction')
+    })
   }
 
   const navRight = (
@@ -31,32 +34,54 @@ export default () => {
   )
   return (
     <div className={styles.expense}>
-      <NavBar onBack={goBack} className="nav-bar-white" right={navRight}>
-        Transaction Detail
-      </NavBar>
-
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className={styles.nav}>
+        <NavBar onBack={goBack} className="nav-bar-white" right={navRight}>
+          Transaction Detail
+        </NavBar>
+      </div>
+      <div className={styles.top}>
+        <div className={styles.amount}>$120</div>
+        <div className={styles['type-desc']}>Buy some grocery</div>
+        <div className={styles.create}>Saturday 4 June 2021 16:20</div>
       </div>
 
-      <div>
-        <div>
-          <div></div>
-          <div></div>
+      <div className={styles['type-card']}>
+        <div className={styles.item}>
+          <div className={styles.type}>Type</div>
+          <div className={styles['type-name']}>Expense</div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.type}>Category</div>
+          <div className={styles['type-name']}>Shopping</div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.type}>Wallet</div>
+          <div className={styles['type-name']}>Wallet</div>
         </div>
       </div>
+      <div className={styles.main}>
+        <div className={styles.line}></div>
+        <div>
+          <span className={styles.title}>Description</span>
+          <p className={styles.desc}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+        <div>
+          <span className={styles.title}>Attachment</span>
+          <img
+            src={ControlMoneyImg}
+            alt="attachment img"
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <Button className="btn-big">Edit</Button>
+      </div>
 
-      <div>
-        <title></title>
-        <div></div>
-      </div>
-      <div>
-        <title></title>
-        <div></div>
-      </div>
-      <Button className="btn-big">Edit</Button>
       <RemoveAlert
         visible={removeVisible}
         onConfirm={handleRemoveConfirm}
