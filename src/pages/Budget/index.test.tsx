d5a0da951890e6de259da1c3f852b8fabecd7b1f
anna_gan <anna_gan@epam.com>
2022-08-09 17:25:43 +0800

    fix: unit test for Budget index test

--- src/pages/Budget/index.test.tsx
+++ src/pages/Budget/index.test.tsx
@@ -1,34 +1,19 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
-import { createMemoryHistory, MemoryHistory } from 'history'
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Budget from '.'
-import { Router } from 'react-router-dom'
 
 describe('test Budget page', () => {
-  let history: MemoryHistory
-  beforeEach(() => {
-    history = createMemoryHistory()
-    history.push('/home')
-  })
-
   it('renders static text', () => {
     render(
-      <Router navigator={history} location={history.location}>
+      <Router>
         <Budget />
       </Router>,
     )
-    expect(screen.getByText('Budget')).toBeInTheDocument()
-    //expect(screen.getByRole('button')).toBeInTheDocument()
-  })
-
-  it('goes back to home page when click the back button', () => {
-    render(
-      <Router navigator={history} location={history.location}>
-        <Budget />
-      </Router>,
-    )
-    // eslint-disable-next-line testing-library/no-node-access
-    fireEvent.click(screen.getByRole('button').firstElementChild!)
-    expect(history.location.pathname).toEqual('/home')
+    expect(
+      screen.getByText(
+        'You don’t have a budget. Let’s make one so you in control.',
+      ),
+    ).toBeInTheDocument()
   })
 })
