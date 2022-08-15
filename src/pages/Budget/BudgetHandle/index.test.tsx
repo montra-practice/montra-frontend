5da32123f23fca3077b058cb6a0792f071bfae53
Vicky He <vicky_he@epam.com>
2022-08-15 10:50:00 +0800

    test: add budget handle UT

--- src/pages/Budget/BudgetHandle/index.test.tsx
+++ src/pages/Budget/BudgetHandle/index.test.tsx
@@ -50,4 +50,39 @@ describe('test budget handle page', () => {
       expect(screen.getByText(/add success/i)).toBeInTheDocument()
     })
   })
+  const mockData = {
+    budgetId: 1,
+    categoryId: '1',
+    categoryName: 'Shopping',
+    budgetTarget: 1000,
+    realCost: 1200,
+    receiveAlert: true,
+    alertPercent: 80,
+    bgColor: '#fff',
+    warning: true,
+    remain: 30,
+    costPercent: 70,
+  }
+  test('should render edit page correctly', () => {
+    setUp({ type: 'edit', data: mockData })
+    expect(screen.getByText('Edit Budget')).toBeInTheDocument()
+    const amountInput = screen.getByPlaceholderText('0')
+    expect(amountInput).toHaveValue(1000)
+    expect(screen.getByText(/shopping/i)).toBeInTheDocument()
+    expect(screen.getByTestId('slider')).toBeInTheDocument()
+  })
+  test('should edit the page successfully', async () => {
+    setUp({ type: 'edit', data: mockData })
+    userEvent.type(screen.getByPlaceholderText('0'), '1200')
+    userEvent.click(screen.getByRole('header'))
+    userEvent.click(screen.getAllByRole('option')[1])
+    expect(screen.getByText(/food/i)).toBeInTheDocument()
+    expect(screen.queryByRole('option')).not.toBeInTheDocument()
+    userEvent.click(screen.getByTestId('switch'))
+    expect(screen.queryByTestId('slider')).not.toBeInTheDocument()
+    userEvent.click(screen.getByText('Continue'))
+    await waitFor(() => {
+      expect(screen.getByText(/update success/i)).toBeInTheDocument()
+    })
+  })
 })
