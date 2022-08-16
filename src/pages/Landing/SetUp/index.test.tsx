3981c4d62ed0f63fe5d4102bb1445cf0ad55a74f
anna_gan <anna_gan@epam.com>
2022-08-16 16:04:15 +0800

    fix: add test case to page Setup

--- src/pages/Landing/SetUp/index.test.tsx
+++ src/pages/Landing/SetUp/index.test.tsx
@@ -1,10 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { createMemoryHistory, MemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
 import SetUp from './index'
 
 describe('test SetUp component', () => {
+  let history: MemoryHistory
+  beforeEach(() => {
+    history = createMemoryHistory()
+    history.push('/user-profile/account/add')
+  })
   test('render SetUp', () => {
-    render(<SetUp />)
+    render(
+      <Router navigator={history} location={history.location}>
+        <SetUp />
+      </Router>,
+    )
 
     expect(screen.getByText('Let’s setup your account!')).toBeInTheDocument()
+  })
+
+  test('jump to create account', () => {
+    render(
+      <Router navigator={history} location={history.location}>
+        <SetUp />
+      </Router>,
+    )
+    fireEvent.click(screen.getByRole('button'))
+    expect(history.location.pathname).toEqual('/user-profile/account/add')
   })
 })
