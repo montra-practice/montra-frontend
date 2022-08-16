41af898133bf04f92cc560fe60b32e1c1aee351e
anna_gan <anna_gan@epam.com>
2022-08-16 15:36:13 +0800

    perf: update UT code in camera.test.tsx

--- src/components/Attachment/camera.test.tsx
+++ src/components/Attachment/camera.test.tsx
@@ -16,15 +16,16 @@ const mockDrawImage = () => {
 }
 
 describe('Test Camera Page', () => {
-  it('Render Camera', () => {
+  beforeEach(() => {
     deviceMock()
+  })
+  it('Render Camera', () => {
     render(<Camera {...mockData} />)
     expect(screen.getByRole('application')).toBeInTheDocument()
     expect(screen.getByAltText('start icon')).toBeInTheDocument()
   })
 
   it('Take a photo', () => {
-    deviceMock()
     render(<Camera {...mockData} />)
     const ctx = mockDrawImage()
     fireEvent.click(screen.getByAltText('start icon'))
@@ -32,14 +33,12 @@ describe('Test Camera Page', () => {
   })
 
   it('On Back', () => {
-    deviceMock()
     render(<Camera {...mockData} />)
     fireEvent.click(screen.getByRole('back'))
     expect(mockData.onBack).toBeCalled()
   })
 
   it('On Complete', () => {
-    deviceMock()
     render(<Camera {...mockData} />)
     mockDrawImage()
     fireEvent.click(screen.getByAltText('start icon'))
